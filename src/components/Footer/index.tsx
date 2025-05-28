2935c2b23dd2563997e359b9ff336aae3a2eb2f4
xuniorss <fortunatojunior2020@gmail.com>
2025-05-27 21:48:49 -0300

    feat: melhorando footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,10 +1,10 @@
 export const Footer = () => {
-  return (
-    <footer className="py-8 px-4 border-t border-gray-800 text-center">
-      <p className="text-gray-400">
-        © 2024 João Silva. Desenvolvido com ❤️ usando React, TypeScript e
-        Tailwind CSS.
-      </p>
-    </footer>
-  );
-};
+   return (
+      <footer className="py-8 px-4 border-t border-gray-800 text-center">
+         <p className="text-gray-400">
+            &copy; {new Date().getFullYear()} Gilberto Júnior. Desenvolvido com
+            ❤️ usando React, TypeScript e Tailwind CSS.
+         </p>
+      </footer>
+   )
+}
